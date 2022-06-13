0a8239fbc39b3b1f3324440d4354771ef523f627
itaiag <itai.agmon@gmail.com>
2022-06-13 16:45:35 +0300

    Tracing

--- src/test/java/playwright/AbstractTest.java
+++ src/test/java/playwright/AbstractTest.java
@@ -1,38 +1,48 @@
 package playwright;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
+import java.nio.file.Paths;
+
 public abstract class AbstractTest {
 
     protected Playwright playwright;
-    protected Browser browser;
+    protected BrowserContext context;
     protected Page page;
 
     @BeforeSuite
     public void beforeSuite(){
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        context = browser.newContext(new Browser.NewContextOptions()
+                .setRecordVideoDir(Paths.get("test-results"+File.separator+"videos/"))
+                .setRecordVideoSize(640, 480));
+        context.tracing().start(new Tracing.StartOptions()
+                .setScreenshots(true)
+                .setSnapshots(true)
+                .setSources(true));
 
     }
 
     @BeforeMethod
     public void beforeMethod(){
-        page = browser.newPage();
+        page = context.newPage();
     }
 
     @AfterMethod
     public void afterMethod(){
         page.close();
     }
+
     @AfterSuite
     public void teardown(){
+        context.tracing().stop(new Tracing.StopOptions()
+                .setPath(Paths.get("test-results"+ File.separator+"trace.zip")));
         playwright.close();
     }
 
--- src/test/java/webdriver/TestOpenProjectFlat.java
+++ src/test/java/webdriver/TestOpenProjectFlat.java
@@ -1,13 +1,9 @@
 package webdriver;
 
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.options.WaitUntilState;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
